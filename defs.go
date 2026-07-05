4b862bf762be05835f72c804524e1052b0aac843
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-07-05 18:07:16 +0200

    Separate logic in files

---
+++ defs.go
@@ -0,0 +1,22 @@
+package main
+
+const (
+	PUSH = 1
+	ADD  = 2
+	SUB  = 3
+	MUL  = 4
+	DIV  = 5
+	OUT  = 6
+	JMP  = 7
+	JNZ  = 8
+)
+
+var program = []int{
+	PUSH, 5,
+	PUSH, 3,
+	ADD,
+	OUT,
+}
+
+var stack []int
+var pc int = 0
---
+++ executor.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func execute(op int) {
+	switch op {
+	case PUSH:
+		stack = append(stack, program[pc+1])
+		pc += 1
+	case ADD:
+		if len(stack) < 2 {
+			return
+		}
+		a := stack[len(stack)-1]
+		b := stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+		stack = append(stack, a+b)
+	case SUB:
+		if len(stack) < 2 {
+			return
+		}
+		a := stack[len(stack)-1]
+		b := stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+		stack = append(stack, a-b)
+	case MUL:
+		if len(stack) < 2 {
+			return
+		}
+		a := stack[len(stack)-1]
+		b := stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+		stack = append(stack, a*b)
+	case DIV:
+		if len(stack) < 2 {
+			return
+		}
+		a := stack[len(stack)-1]
+		b := stack[len(stack)-2]
+		if b == 0 {
+			return
+		}
+		stack = stack[:len(stack)-2]
+		stack = append(stack, a/b)
+	case OUT:
+		fmt.Println(stack)
+	case JMP:
+		pc = program[pc+1]
+	case JNZ:
+		if stack[len(stack)] == 0 {
+			pc = program[pc+1]
+		}
+
+	}
+}
